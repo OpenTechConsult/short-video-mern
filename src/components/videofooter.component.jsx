c91c703e1880ab2485152f1494013c63e4c2e4dc
OpenTech-Consult <opentech.consult@gmail.com>
2022-04-16 19:56:46 +0000

    Video footer component
    added the class videoFooter__text to the div whose parent is div.videoFooter

--- src/components/videofooter.component.jsx
+++ src/components/videofooter.component.jsx
@@ -6,7 +6,7 @@ import './videofooter.styles.css';
 const VideoFooter = () => {
     return (
         <div className="videoFooter">
-            <div className="">
+            <div className="videoFooter__text">
                 <h3>@nabendu82</h3>
                 <p>Macbook Air to new Windows editing beast.</p>
                 <div className="videoFooter__ticker">
